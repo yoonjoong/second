82bb2399127088a2600307cf7ec8c906dbaff850
yoonjoong <yoonjoong311@gmail.com>
2019-07-30 12:22:17 +0900

    add project name

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -133,3 +133,6 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+import django_heroku
+django_heroku.settings(locals())
